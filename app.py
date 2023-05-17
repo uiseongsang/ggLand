63b4db953c6bddd709cb446675ed04069c4e46f9
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-05-17 19:48:59 +0900

    GET & POST 취합 완료

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
+from datetime import datetime
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@limchaeyoung.boi4rwj.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+import certifi
+ca=certifi.where()
+
+client = MongoClient('mongodb+srv://sparta:test@cluster0.fzjk15v.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.guestbook
 
 @app.route('/')
 def home():
     return render_template('detail.html')
 
-@app.route("/member", methods=["POST"])
-def member_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg':'POST 연결 완료!'})
+@app.route("/api/comment", methods=["POST"])
+def comment_post():
+    #commentId_receive = request.form['commentId_give']
+    commentName_receive = request.form['commentName_give']
+    commentContent_receive = request.form['commentContent_give']
+    date = str(datetime.now()) 
+    print(commentName_receive,commentContent_receive,date)
+
+    doc = {
+        #'commentId':commentId_receive,
+        'commentName':commentName_receive,
+        'commentDate': date,
+        'commentContent':commentContent_receive}
+    
+    db.guestbook.insert_one(doc)
+    return jsonify({'msg':'아일랜드에 발자취를 남겼다!'})
 
 #방명록 내용 출력
 @app.route("/api/comment", methods=["GET"])
-def member_get():    
-    all_comment = list(db.cy_post.find({},{'_id':False}))
-    return jsonify({'result': all_comment})
+def comments_get():    
+    all_comments = list(db.guestbook.find({},{'_id':True, 'commentId':True, 'commentName':True, 'commentDate':True, 'commentContent':True}))
+    
+    for comment in all_comments:
+        comment['_id'] = str(comment['_id'])
+    #print(all_comments)
+    return jsonify({'result': all_comments})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5003, debug=True)
--- static/js/detailJS.js
+++ static/js/detailJS.js
@@ -2,29 +2,48 @@ $(document).ready(function(){
     show_Comment();
   });
 
-  function show_Comment() {
-      fetch('/api/comment').then((res) => res.json()).then((data) => {
-        let rows = data['result'];
 
-        $('#comments').empty();
-        rows.forEach((a)=> {
-            let name = a['commentName'];
-            let date = a['commentDate'];
-            let content = a['commentContent'];
+function posting() {
+    //let commentId = $('#commentId').val()
+    let commentName = $('#commentName').val()
+    let commentContent = $('#commentContent').val()
 
-            console.log(name,date,content)
-            let temp_html =`<div class="comment1">
-                                <div class="comments">
-                                    ${date} &nbsp; ${name} :  " ${content} "
-                                </div>
-                                <div class="btn-area">
-                                <button class="update-btn" onclick="location.href='/'">수정</button> <button class="delete-btn" onclick="">삭제</button>
-                                </div>
+    let formData = new FormData();
+    //formData.append("commentId_give", commentId);
+    formData.append("commentName_give", commentName);
+    formData.append("commentContent_give", commentContent);
+
+    fetch("/api/comment", { method: "POST", body: formData })
+        .then((res) => res.json())
+        .then((data) => {
+            alert(data["msg"]);
+            window.location.reload();
+        });
+}
+
+function show_Comment() {
+    fetch("/api/comment").then((res) => res.json()).then((data) => {
+    let rows = data['result'];
+
+    $('#comments').empty();
+    rows.forEach((a)=> {
+        let name = a['commentName'];
+        let date = a['commentDate'];
+        let content = a['commentContent'];
+
+        //console.log(name,date,content)
+        let temp_html =`<div class="comment1">
+                            <div class="comments">
+                                ${date} &nbsp; ${name} :  " ${content} "
                             </div>
-                            <hr/>`
+                            <div class="btn-area">
+                            <button class="update-btn" onclick="location.href='/'">수정</button> <button class="delete-btn" onclick="">삭제</button>
+                            </div>
+                        </div>
+                        <hr/>`
 
-            $('#comments').append(temp_html);
+        $('#comments').append(temp_html);
 
-        })
-      })
-  }
+    })
+    })
+}
