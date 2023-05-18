17047e53d8825cf7f9414e77bf073293a424893f
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-05-18 16:23:46 +0900

    수정 API 완료(디자인 수정 해야함)

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, jsonify, redirect
 from datetime import datetime
+from bson.objectid import ObjectId
 app = Flask(__name__)
 
 from pymongo import MongoClient
 import certifi
 ca=certifi.where()
 
+# 상의성: mongodb+srv://sparta:test@cluster0.fzjk15v.mongodb.net/?retryWrites=true&w=majority
 client = MongoClient('mongodb+srv://sparta:test@cluster0.fzjk15v.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.guestbook
 
@@ -15,14 +17,12 @@ def home():
 
 @app.route("/api/comment", methods=["POST"])
 def comment_post():
-    #commentId_receive = request.form['commentId_give']
     commentName_receive = request.form['commentName_give']
     commentContent_receive = request.form['commentContent_give']
     date = str(datetime.now()) 
-    print(commentName_receive,commentContent_receive,date)
+    #print(commentName_receive,commentContent_receive,date)
 
     doc = {
-        #'commentId':commentId_receive,
         'commentName':commentName_receive,
         'commentDate': date,
         'commentContent':commentContent_receive}
@@ -30,15 +30,30 @@ def comment_post():
     db.guestbook.insert_one(doc)
     return jsonify({'msg':'아일랜드에 발자취를 남겼다!'})
 
+#방명록 내용 수정
+@app.route("/api/update-comment", methods=["POST"])
+def comment_update():
+    receive_id = str(request.form['_id_give'])
+    receive_target_name = request.form['targetname_give']
+    receive_target_content = request.form['targetcontent_give']
+    #print(receive_id,receive_target_name,receive_target_content)
+
+    # string에서 ObjectId로 변경
+    comment_id = ObjectId(receive_id)
+    db.guestbook.update_one({'_id': comment_id}, {'$set': {'commentName': receive_target_name, 'commentContent': receive_target_content}})
+    return jsonify({'msg':'나의 발자취를 고쳤다!'})
+
 #방명록 내용 출력
 @app.route("/api/comment", methods=["GET"])
-def comments_get():    
-    all_comments = list(db.guestbook.find({},{'_id':True, 'commentId':True, 'commentName':True, 'commentDate':True, 'commentContent':True}))
-    
+def comments_get():
+    ## 부분 별로 True, false 가능!!
+    #list(db.guestbook.find({},{'_id':True, 'commentName':True, 'commentDate':True, 'commentContent':True}))    
+    all_comments = list(db.guestbook.find({}))
     for comment in all_comments:
         comment['_id'] = str(comment['_id'])
     #print(all_comments)
     return jsonify({'result': all_comments})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5003, debug=True)
--- static/js/detailJS.js
+++ static/js/detailJS.js
@@ -1,15 +1,69 @@
-$(document).ready(function(){
+$(document).ready(function() {
     show_Comment();
-  });
 
+    $(document).on("click", ".update-btn", function() {
+        var entry = $(this).closest(".comment1");
+        var content = entry.find(".comments");
+        var editForm = entry.find(".edit-form");
+        var editBtn = entry.find(".update-btn");
+        var deleteBtn = entry.find(".delete-btn");
+
+        var name = content.find("p:first-child").text();
+        var msg = content.find("p:last-child").text();
+
+        editForm.find("input[name='name']").val(name);
+        editForm.find("textarea[name='message']").val(msg);
+
+        deleteBtn.hide();
+        editBtn.hide();
+        content.hide();
+        editForm.show();
+    });
+
+    $(document).on("click", ".cancel-btn", function(e) {
+        e.preventDefault();
+        var entry = $(this).closest(".comment1");
+        var content = entry.find(".comments");
+        var editForm = entry.find(".edit-form");
+        var editBtn = entry.find(".update-btn");
+        var deleteBtn = entry.find(".delete-btn");
+
+        editForm.hide();
+        deleteBtn.show();
+        editBtn.show();
+        content.show();
+    });
+
+    $(document).on("click", ".submit-btn", function(e) {
+        e.preventDefault();
+        var entry = $(this).closest(".comment1");
+        var editForm = entry.find(".edit-form");
+        var name = editForm.find("input[name='name']").val();
+        var msg = editForm.find("textarea[name='message']").val();
+        var id = entry.find(".submit-btn").data("id");
+        console.log(name, msg)
+        console.log("hi")
+        console.log(id)
+
+        let formData = new FormData();
+        formData.append("_id_give", id);
+        formData.append("targetname_give", name);
+        formData.append("targetcontent_give", msg);
+
+        fetch('/api/update-comment', { method: "POST", body: formData})
+            .then((res) => res.json())
+            .then((data) => {
+                alert(data["msg"]);
+                window.location.reload();
+            });
+    });
+});
 
 function posting() {
-    //let commentId = $('#commentId').val()
-    let commentName = $('#commentName').val()
-    let commentContent = $('#commentContent').val()
+    let commentName = $('#commentName').val();
+    let commentContent = $('#commentContent').val();
 
     let formData = new FormData();
-    //formData.append("commentId_give", commentId);
     formData.append("commentName_give", commentName);
     formData.append("commentContent_give", commentContent);
 
@@ -22,28 +76,39 @@ function posting() {
 }
 
 function show_Comment() {
-    fetch("/api/comment").then((res) => res.json()).then((data) => {
-    let rows = data['result'];
+    fetch("/api/comment")
+        .then((res) => res.json())
+        .then((data) => {
+            let rows = data['result'];
 
-    $('#comments').empty();
-    rows.forEach((a)=> {
-        let name = a['commentName'];
-        let date = a['commentDate'];
-        let content = a['commentContent'];
-
-        //console.log(name,date,content)
-        let temp_html =`<div class="comment1">
-                            <div class="comments">
-                                ${date} &nbsp; ${name} :  " ${content} "
-                            </div>
-                            <div class="btn-area">
-                            <button class="update-btn" onclick="location.href='/'">수정</button> <button class="delete-btn" onclick="">삭제</button>
-                            </div>
+            $('#comments').empty();
+            rows.forEach((a) => {
+                let _id = a['_id'];
+                let name = a['commentName'];
+                let date = a['commentDate'];
+                let content = a['commentContent'];
+                // console.log("ID is ")
+                // console.log(_id)
+                let temp_html = `
+                    <div class="comment1">
+                        <div class="comments">
+                            <p>${name}</p>
+                            <p>${content}</p>
+                            ${date}&nbsp;  
                         </div>
-                        <hr/>`
-
-        $('#comments').append(temp_html);
-
-    })
-    })
+                        <div class="btn-area">
+                            <button class="update-btn">수정</button>
+                            <button class="delete-btn" onclick="">삭제</button>
+                            <form class="edit-form" style="display: none;">
+                                <input type="text" name="name" placeholder="유저 이름" value="${name}" required>
+                                <textarea name="message" placeholder="내용" required>${content}</textarea>
+                                <button class="submit-btn" data-id="${_id}" >저장</button>
+                                <button class="cancel-btn">취소</button>
+                            </form>
+                        </div>
+                    </div>
+                `;
+                $('#comments').append(temp_html);
+            });
+        });
 }
